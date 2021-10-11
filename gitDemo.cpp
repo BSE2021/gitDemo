197795078b806f8920432dfe0cd2f7764c559cf2
21wdwu02 <beulathamban@gmail.com>
2021-10-11 13:59:21 +1300

    git demo message added

---
+++ gitDemo.cpp
@@ -0,0 +1,10 @@
+// gitDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\nGit and Github demo";
+}
