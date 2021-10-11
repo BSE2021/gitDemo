9f446e688f09b2c5639902e5dde90771f121fb7b
21wdwu02 <beulathamban@gmail.com>
2021-10-11 14:19:39 +1300

    updated at home

--- gitDemo.cpp
+++ gitDemo.cpp
@@ -8,4 +8,5 @@ int main()
 {
     cout << "\nGit and Github demo";
     cout << "\nI am updating my project. So I need to push this into github";
+    cout << "\nFrom home, I am working in this project. Then going to push into github";
 }
